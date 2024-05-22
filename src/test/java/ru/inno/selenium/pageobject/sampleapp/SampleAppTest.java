1f96cd1c48bc820433ba4de9d6626817340ceca8
Dmitry Eremin <mitya1705@gmail.com>
2024-05-22 21:41:51 +0300

    containers

--- src/test/java/ru/inno/selenium/pageobject/sampleapp/SampleAppTest.java
+++ src/test/java/ru/inno/selenium/pageobject/sampleapp/SampleAppTest.java
@@ -1,28 +1,36 @@
 package ru.inno.selenium.pageobject.sampleapp;
 
-import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.VncRecordingContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Epic("Пользователи")
-@Feature("Блокировка пользователей")
-@Story("Как админ, я могу блокировать пользователя, чтобы ограничить ему доступ")
-@Owner("Дмитрий Еремин")
-@DisplayName("Тесты на SampleApp")
-@ExtendWith(TgWatcher.class)
+@Testcontainers
 public class SampleAppTest {
 
     private WebDriver driver;
     private SampleAppPage page;
 
+    @Container
+    public BrowserWebDriverContainer<?> container = new BrowserWebDriverContainer<>()
+            .withCapabilities(new FirefoxOptions())
+            .withExposedPorts(7900)
+            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, Path.of("target").toFile(), VncRecordingContainer.VncRecordingFormat.MP4);
+
     @BeforeEach
-    public void setDriver() {
-        driver = new ChromeDriver();
+    public void setDriver() throws MalformedURLException {
+        driver = new RemoteWebDriver(container.getSeleniumAddress(), new FirefoxOptions());
         page = new SampleAppPage(driver);
     }
 
@@ -34,11 +42,6 @@ public class SampleAppTest {
     }
 
     @Test
-    @DisplayName("Успешная авторизация")
-    @Description("Проверяем успешную авторизацию с паролем pwd")
-    @Tags({@Tag("авторизация"), @Tag("позитивный")})
-    @Severity(SeverityLevel.BLOCKER)
-    @TmsLink("INNO-137")
     public void happyPathTest() {
         page.open();
         page.auth("Inno", "pwd");
@@ -46,10 +49,6 @@ public class SampleAppTest {
     }
 
     @Test
-    @DisplayName("Вход с неправильным паролем")
-    @Severity(SeverityLevel.CRITICAL)
-    @Tags({@Tag("авторизация"), @Tag("негативной")})
-    @TmsLink("INNO-138")
     public void wrongPassTest() {
         String login = "Inno";
         String pass = "qwerty";
@@ -62,13 +61,6 @@ public class SampleAppTest {
     }
 
     @Test
-    @DisplayName("Сабмит пустой формы")
-    @Description("Проверяем, что нельзя зайти в ЛК с пустыми логином и паролем")
-    @Severity(SeverityLevel.BLOCKER)
-    @Tags({@Tag("авторизация"), @Tag("негативный")})
-    @TmsLink("INNO-142")
-    @Issue("JIRA-63")
-    @Link(name = "how to", url = "https://ya.ru")
     public void blankLoginAndPass() {
         page.open();
         page.clickLogin();
